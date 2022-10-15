2b42551a89f0aaacd71b4b600440f7b3eeb6c226
Lakshit Khanna <lakshitkhanna311@gmail.com>
2022-10-15 10:07:27 +0530

    class 15-10-2022

---
+++ lecture8/script.js
@@ -0,0 +1,42 @@
+// 'use strict';
+// alert("Hello ");
+
+
+// abc = () => {
+//     document.getElementById("title").innerText = "HELLO";
+// }
+
+
+let person = {
+    "firstName" : "Lakshit",
+    lastName : "Khanna",
+    contact : "10",
+}
+
+
+console.log(`${person.firstName} ${person["lastName"]} => ${person.contact}`);
+
+let a = [1,2,3,4,5,"hello",true];
+// for(i=0;i<a.length;i++){
+//     console.log(a[i]);
+// }
+
+
+// for(let e of a){
+//     console.log("Hello",e);
+// }
+
+
+
+// for(let key in person){
+//     console.log("In" , key , ": ", person[key]);
+// }
+
+
+
+
+
+
+
+
+
